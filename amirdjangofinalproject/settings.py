e0c0d35dd320968503c3578b2ebe40573a81b263
Amirhossein Fathollahi <dj1372@gmail.com>
2022-09-07 12:57:23 +0430

    static and media setting
    + static root
    + static url
    + media root
    + media url

--- amirdjangofinalproject/settings.py
+++ amirdjangofinalproject/settings.py
@@ -113,10 +113,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
