cf9c37406f45d0e2ddd6923ff936d51de7573957
Amirhossein Fathollahi <dj1372@gmail.com>
2022-09-12 21:19:50 +0430

    paginator configured

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
 
-def blog_home(request):
+def blog_home(request, **kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') is not None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') is not None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -16,3 +30,10 @@ def blog_single(request, pid):
     return render(request, 'blog/blog-single.html', context)
 
 
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
